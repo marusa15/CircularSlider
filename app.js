e4a4b7c8b3d362edceeac10bda65df5b91712bdc
Marusa <marusa15@gmail.com>
2017-11-24 16:26:42 +0100

    Added function for displaying multiple circles.

--- app.js
+++ app.js
@@ -14,27 +14,53 @@ var options = [
           maxValue: '1000',
           minValue: '250',
           step: '5',
-          radius: '30px',
+          radius: '20px',
+          category: 'Food'
+         },
+         {
+          container: 'con1',
+          color: 'red',
+          maxValue: '1000',
+          minValue: '250',
+          step: '5',
+          radius: '60px',
           category: 'Food'
          }
         ]
 
+ 
 var displayCircle = function(options, element) {
-  var circleElement='';
-  var circleElement='<div id="circle"><div id="slider"></div></div><div class="instructions">' +
-  'Adjust dial to enter expenses</div>';  
-  
-  return element.html(circleElement);
-}
+  var circleInstance = $('<div id="circle-1"><div id="slider-1"></div></div>');
+
+  for(var i = 2; i <= options.length; i++) {
+    circleInstance = circleInstance.append('<div id="circle-' 
+      + i + '"><div id="slider-' + i +
+     '"></div></div>');
+    
+  }
+    element.append(circleInstance);
+  } 
 
 displayCircle(options, $('.js-container'));
 
 var addCSSProperties = function(options) {
-  var circle = document.querySelector('#circle');
+  var circle = document.querySelector('#circle-1');
 
   circle.style.setProperty('border', '2px solid ' + options[0].color);
   circle.style.setProperty('width', options[0].radius);
   circle.style.setProperty('height', options[0].radius);
+
+  var circle = document.querySelector('#circle-2');
+
+  circle.style.setProperty('border', '2px solid ' + options[1].color);
+  circle.style.setProperty('width', options[1].radius);
+  circle.style.setProperty('height', options[1].radius);
+
+  var circle = document.querySelector('#circle-3');
+
+  circle.style.setProperty('border', '2px solid ' + options[2].color);
+  circle.style.setProperty('width', options[2].radius);
+  circle.style.setProperty('height', options[2].radius);
 }
 
 addCSSProperties(options);
@@ -56,20 +82,20 @@ displayInput(options, $('.js-container__left'));
 
 
 var circulate = function() {
-        var $container = $('#circle'); //?
-        var $slider = $('#slider');
+        var $container = $('#circle-1'); //?
+        var $slider = $('#slider-1');
     
         var sliderW2 = $slider.width()/2;  // shrani v spremenljivko širino drsne ploščice brez enote in deli z dva (radij)
         var sliderH2 = $slider.height()/2;  
 
         var radius = 15; //radij vrtenja?
         var deg = 0;    
-        var elP = $('#circle').offset();
+        var elP = $('#circle-1').offset();
         var elPos = { x: elP.left, y: elP.top}; // shrani v object koordinate el. #circle, kroznice
         var X = 0, Y = 0;
         var mdown = false;
 
-        $('#circle').mousedown(function (e){ mdown = true; })
+        $('#circle-1').mousedown(function (e){ mdown = true; })
                     .mouseup(function (e) { mdown = true; })
                     .mousemove(function (e) {
                         if (mdown) {
