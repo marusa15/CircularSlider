59ca6967fc55f7b50d9e74bb70b697516de62963
Marusa <marusa15@gmail.com>
2017-12-03 13:06:28 +0100

    Drew a grey background circle with canvas.

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var options = [
       		minValue: '0',
       		step: '1',
       		radius: '30px',
-          category: 'Transport'
+          category: 'Transport',
+          clicked: 'false'
 	       },
          {
           container: 'con1',
@@ -14,8 +15,9 @@ var options = [
           maxValue: '1000',
           minValue: '250',
           step: '1',
-          radius: '100px',
-          category: 'Food'
+          radius: '60px',
+          category: 'Food',
+          clicked: 'false'
          },
          {
           container: 'con1',
@@ -23,8 +25,9 @@ var options = [
           maxValue: '1000',
           minValue: '250',
           step: '5',
-          radius: '60px',
-          category: 'Food'
+          radius: '100px',
+          category: 'Food',
+          clicked: 'false'
          }
         ]
 
@@ -50,17 +53,41 @@ var addCSSProperties = function(options) {
   circle.style.setProperty('height', options[2].radius);
 }
 
-addCSSProperties(options);
+var clickCircle = function(options, item) {
+
+  currentItem = getItem(options, item);
+  options.Current = item;
+  options[item].clicked = true;
+
+}
+
+function getItem(options, itemIndex) {
+    return options[itemIndex];    
+}
+
 
 
 // display functions
 
+function draw() {
+        var canvas = document.getElementById('c-circle');
+        var ctx = canvas.getContext('2d');
+        ctx.strokeStyle = '#afb1b5';
+        ctx.lineWidth = 15;
+        ctx.setLineDash([5, 1]);
+        ctx.lineDashOffset = 5;
+        ctx.beginPath();
+        ctx.arc(95,95,40,0,2*Math.PI);
+        ctx.stroke();
+
+      }
+
  
 var displayCircle = function(options, element) {
-  var circleInstance = $('<div id="circle-1"><div id="slider-1" class="slider"></div></div>');
+  var circleInstance = $('<div class="circle" id="circle-1" circle-id="1"><div id="slider-1" class="slider"></div></div>');
 
   for(var i = 2; i <= options.length; i++) {
-    circleInstance = circleInstance.append('<div id="circle-' 
+    circleInstance = circleInstance.append('<div class="circle" circle-id="' + i + '" id="circle-' 
       + i + '"><div class="slider" id="slider-' + i +
      '"></div></div>');
     
@@ -68,7 +95,7 @@ var displayCircle = function(options, element) {
     element.append(circleInstance);
   } 
 
-displayCircle(options, $('.js-container'));
+
 
 
 var displayInput = function(options, element) {
@@ -82,56 +109,4 @@ var displayInput = function(options, element) {
     return element.html(inputElement);
 } 
 
-displayInput(options, $('.js-container__left')); 
-
-// event listeners
-
-var circulate = function(options, i) {
-        var $slider = $('#slider-' + i);
-    
-        var sliderW2 = $slider.width()/2;  // shrani v spremenljivko širino drsne ploščice brez enote in deli z dva (radij)
-        var sliderH2 = $slider.height()/2;  
-        
-        var diameter = parseInt(options[i-1].radius, 10);
-        var radius = diameter/2;
-        var deg = 0;
-        console.log('deg: ', deg);    
-        var elP = $('#circle-' + i).offset();
-        var elPos = { x: elP.left, y: elP.top}; // shrani v object koordinate el. #circle, kroznice
-        var X = 0, Y = 0;
-        var mdown = false;
-
-        $('#circle-' + i).mousedown(function (event){ mdown = true; })
-                    .mouseup(function (event) { mdown = true; })
-                    .mousemove(function (event) {
-                        if (mdown) {
-                           var mPos = {x: e.clientX-elPos.x, y: e.clientY-elPos.y}; // polozaj x in y koordinate miske
-                           var atan = Math.atan2(mPos.x-radius, mPos.y-radius); // Math.atan2() vrne kot med x osjo in točko (x,y)
-                           deg = -atan/(Math.PI/180) + 180; // final (0-360 positive) degrees from mouse position 
-                            
-                           X = Math.round(radius* Math.sin(deg*Math.PI/180));    
-                           Y = Math.round(radius*  -Math.cos(deg*Math.PI/180));
-
-                           $slider.css({ left: X+radius-sliderW2, top: Y+radius-sliderH2 });              
-                           // AND FINALLY apply exact degrees to ball rotation
-                           $slider.css({ WebkitTransform: 'rotate(' + deg + 'deg)'});
-                           $slider.css({ '-moz-transform': 'rotate(' + deg + 'deg)'});
-                           //
-                           // PRINT DEGREES
-                           $('input[name="angle"]').val(Math.ceil(deg));
-                        }
-                    });
-}
-
-
-
-
-
-
-$(function(){ 
-  circulate(options, 1);
-  circulate(options, 2);
-  circulate(options, 3);
-
-});
 
