e36e4742ceb16899fa75d3867127bd4142871cb0
Marusa <marusa15@gmail.com>
2017-12-08 13:17:04 +0100

    Refactored the background circle-drawing function.

--- app.js
+++ app.js
@@ -31,44 +31,42 @@ var options = [
          }
         ]
 
-
-
-var clickCircle = function(options, item) {
-
-  currentItem = getItem(options, item);
-  options.Current = item;
-  options[item].clicked = true;
-
-}
-
-function getItem(options, itemIndex) {
-    return options[itemIndex];    
-}
-
-
-
 // display functions
 
 document.body.onload = function() {
-drawCircle();
+
+myCircle.draw(100, 100, 70);
 drawSlider(100, 30);
+
 };
 
-
-function drawCircle(radius, x_slider, y_slider) {
-        var canvas = document.getElementById('c-circle');
-        var ctx = canvas.getContext('2d');
-            
-     
-        ctx.strokeStyle = '#afb1b5';
-        ctx.lineWidth = 15; // does not affect the radius, goes 7.5 out and 7.5 in
-        ctx.setLineDash([5, 1]);
-        ctx.lineDashOffset = 5;
-        ctx.beginPath();
-        ctx.arc(100,100,radius,-0.5*Math.PI,2*Math.PI);
-
-        ctx.stroke();
+function Circle(x, y, radius) {
+  this.x = x || 100;
+  this.y = y || 100;
+  this.radius = radius || 70;
+  this.startAngle = -0.5*Math.PI;
+  this.endAngle = 2*Math.PI;
+  
 }
+
+var myCircle = new Circle();
+
+// Draws this shape to a given context
+Circle.prototype.draw = function(ctx) {
+          var canvas = document.getElementById('c-circle');
+          var ctx = canvas.getContext('2d');
+              
+       
+          ctx.strokeStyle = '#afb1b5';
+          ctx.lineWidth = 15; // does not affect the radius, goes 7.5 out and 7.5 in
+          ctx.setLineDash([5, 1]);
+          ctx.lineDashOffset = 5;
+          ctx.beginPath();
+          ctx.arc(100,100,this.radius,-0.5*Math.PI,2*Math.PI);
+
+          ctx.stroke();
+}
+
 
         // sliding button
 function drawSlider(X,Y) {
@@ -90,16 +88,6 @@ function drawSlider(X,Y) {
         ctx.stroke();
 }
 
-function cutSlider(x, y, radius){
-    var canvas = document.getElementById('c-circle');
-    var ctx = canvas.getContext('2d');
-
-    ctx.globalCompositeOperation = 'destination-out'
-    ctx.beginPath();
-    ctx.arc(100, 30, 9.5, startAngle, endAngle);
-    ctx.fill();
-}
-
 
         // part of circle in color
 function colorCircle(hue, radius, part) {
@@ -117,11 +105,145 @@ function colorCircle(hue, radius, part) {
         ctx.stroke();        
 }
 
-       
+ 
+      
     
 
-drawCircle(70);
+/*
 
+var canvasState = function(canvas) {
+        this.valid = false; // when set to true, the canvas will redraw everything
+        this.shapes = [];  // the collection of things to be drawn - imeli bom več krogov različnih
+        this.dragging = false; // Keep track of when we are dragging
+        // the current selected object.
+        // In the future we could turn this into an array for multiple selection
+        this.selection = null;
+        this.dragoffx = 0; // See mousedown and mousemove events for explanation
+        this.dragoffy = 0;
+
+        // Right here "this" means the CanvasState. But we are making events on the Canvas itself,
+        // and when the events are fired on the canvas the variable "this" is going to mean the canvas!
+        // Since we still want to use this particular CanvasState in the events we have to save a reference to it.
+        // This is our reference!
+        var myState = this;
+        
+        //fixes a problem where double clicking causes text to get selected on the canvas
+        canvas.addEventListener('selectstart', function(e) { e.preventDefault(); return false; }, false); //
+        // Up, down, and move are for dragging
+        canvas.addEventListener('mousedown', function(e) {
+          var mouse = myState.getMouse(e);
+          var mx = mouse.x;
+          var my = mouse.y;
+          var shapes = myState.shapes;
+          var l = shapes.length;
+          for (var i = l-1; i >= 0; i--) {
+            if (shapes[i].contains(mx, my)) {
+              var mySel = shapes[i];
+              // Keep track of where in the object we clicked
+              // so we can move it smoothly (see mousemove)
+              myState.dragoffx = mx - mySel.x;
+              myState.dragoffy = my - mySel.y;
+              myState.dragging = true;
+              myState.selection = mySel;
+              myState.valid = false;
+              return;
+            }
+          }
+          // havent returned means we have failed to select anything.
+          // If there was an object selected, we deselect it
+          if (myState.selection) {
+            myState.selection = null;
+            myState.valid = false; // Need to clear the old selection border
+          }
+        }, true);
+        canvas.addEventListener('mousemove', function(e) {
+          if (myState.dragging){
+            var mouse = myState.getMouse(e);
+            // We don't want to drag the object by its top-left corner,
+            // we want to drag from where we clicked.
+            // Thats why we saved the offset and use it here
+            myState.selection.x = mouse.x - myState.dragoffx;
+            myState.selection.y = mouse.y - myState.dragoffy;   
+            myState.valid = false; // Something's dragging so we must redraw
+          }
+        }, true);
+        canvas.addEventListener('mouseup', function(e) {
+            myState.dragging = false;
+          }, true);
+        // add the click event as well
+
+        // **** Options! ****
+          
+          
+          this.interval = 30;
+          setInterval(function() { myState.draw(); }, myState.interval);
+
+          // While draw is called as often as the INTERVAL variable demands,
+          // It only ever does something if the canvas gets invalidated by our code
+          CanvasState.prototype.draw = function() {
+            // if our state is invalid, redraw and validate!
+            if (!this.valid) {
+              var ctx = this.ctx;
+              var shapes = this.shapes;
+              this.clear();
+              
+              // ** Add stuff you want drawn in the background all the time here **
+              // grey circle
+              
+              // draw all shapes
+              var l = shapes.length;
+              for (var i = 0; i < l; i++) {
+                var shape = shapes[i];
+                // We can skip the drawing of elements that have moved off the screen:
+                if (shape.x > this.width || shape.y > this.height ||
+                    shape.x + shape.w < 0 || shape.y + shape.h < 0) continue;
+                shapes[i].draw(ctx);
+              }
+              
+              // draw selection
+              // right now this is just a stroke along the edge of the selected Shape
+              if (this.selection != null) {
+                ctx.strokeStyle = this.selectionColor;
+                ctx.lineWidth = this.selectionWidth;
+                var mySel = this.selection;
+                ctx.strokeRect(mySel.x,mySel.y,mySel.w,mySel.h);
+              }
+              
+              // ** Add stuff you want drawn on top all the time here **
+              
+              this.valid = true;
+            }
+
+            // Creates an object with x and y defined,
+
+              // set to the mouse position relative to the state's canvas
+              // If you wanna be super-correct this can be tricky,
+              // we have to worry about padding and borders
+              CanvasState.prototype.getMouse = function(e) {
+                var element = this.canvas, offsetX = 0, offsetY = 0, mx, my;
+                
+                // Compute the total offset
+                if (element.offsetParent !== undefined) {
+                  do {
+                    offsetX += element.offsetLeft;
+                    offsetY += element.offsetTop;
+                  } while ((element = element.offsetParent));
+                }
+
+                // Add padding and border style widths to offset
+                // Also add the offsets in case there's a position:fixed bar
+                offsetX += this.stylePaddingLeft + this.styleBorderLeft + this.htmlLeft;
+                offsetY += this.stylePaddingTop + this.styleBorderTop + this.htmlTop;
+
+                mx = e.pageX - offsetX;
+                my = e.pageY - offsetY;
+                
+                // We return a simple javascript object (a hash) with x and y defined
+                return {x: mx, y: my};
+              }
+                        }
+}
+*/
 
 var moveSlider = function(element) {
 
@@ -147,7 +269,7 @@ var moveSlider = function(element) {
 
 
 
-        $('#c-circle').click(function (event) {
+        $('#c-circle').click(function (event) { // later on add mousemove, mouseup and mousedown
                         if (mdown) {
                            var mPos = {x: event.clientX-elPos.x-30, y: event.clientY-elPos.y-30}; // polozaj x in y koordinate miske
 
