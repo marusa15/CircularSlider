0f9eb1743e52adc5b817ffa467ab7c29bc81cce0
Marusa <marusa15@gmail.com>
2017-12-12 11:16:24 +0100

    Fixed the indentation and spacing in js file.

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 var canvas = document.getElementById('c-circle');
 x0 = canvas.width / 2;
 y0 = canvas.height / 2;
+var sliderW2 = 4.75;
+var sliderH2 = 4.75;
+var radius;  
+var radiusMax = radius + sliderH2;
+var radiusMin = radius - sliderH2;
 
 
 var options = [
 	       {
-      		container: 'con1',
-      		color: 'blue',
-      		maxValue: '100',
-      		minValue: '0',
-      		step: '1',
-      		radius: '30', //in pixels
+  		  container: 'con1',
+  		  color: 'blue',
+  		  maxValue: '100',
+  		  minValue: '0',
+  		  step: '1',
+  		  radius: '30', //in pixels
           category: 'Transport',
           part: -0.5*Math.PI,
           sliderX: x0,
@@ -73,21 +78,12 @@ var options = [
         ]
 
 // state modification functions
-var sliderW2 = 4.75;
-var sliderH2 = 4.75;
-var radius;  
-var radiusMax = radius + sliderH2;
-var radiusMin = radius - sliderH2;
+
+
     
 // display functions
 
-document.body.onload = function(radius) {
 
-  drawCircle(radius);
-  drawSlider(x0, y0 - radius); 
-
-
-};
 
 function drawCircle(radius) {
         var canvas = document.getElementById('c-circle');
@@ -104,7 +100,7 @@ function drawCircle(radius) {
 }
 
 
-        // sliding button
+       
 function drawSlider(X,Y) {
         var canvas = document.getElementById('c-circle');
         var ctx = canvas.getContext('2d');
@@ -125,6 +121,7 @@ function drawSlider(X,Y) {
         ctx.stroke();
 }
 
+
 function clear() {
     var canvas = document.getElementById('c-circle');
     var ctx = canvas.getContext('2d');
@@ -133,7 +130,7 @@ function clear() {
 }
 
 
-        // part of circle in color
+        
 function colorCircle(hue, radius, part) {
         var canvas = document.getElementById('c-circle');
         var ctx = canvas.getContext('2d');
@@ -149,150 +146,71 @@ function colorCircle(hue, radius, part) {
         ctx.stroke();        
 }
 
-
-
-function CanvasState(canvas) {
-  this.canvas = canvas;
-  this.valid = false; 
-  var myState = this; 
-  this.interval = 30; // maybe this would be useful to remove black square appearing in mobile
-  setInterval(function(){ myState.draw(); }, myState.interval);
-
-}
-
-
 // event listeners
 
 
-
-
 var moveSlider = function(element) {
-        
-
-        
-        var deg = 0;
+     //   var deg = 0;
         var elP = element.offset();  //razmisli, na kakšen drugi način bi se to dalo rešit
         var elPos = { x: elP.left, y: elP.top};   //dobi koordinate kvadrata 200x200 glede na dokument 
-        console.log('elpos', elPos);
-        
-        
-        var X = 0, Y = 0;
+    //    var X = 0, Y = 0;
         var mdown = false;
 
 
-        var radius;
-        var sliderW2 = 4.75;
-        var sliderH2 = 4.75;
-        
-       
-     
-        // Determine if a point is inside the circle's bounds
-        var circleContains = function(x, y) {
+        function circleContains(x, y) { // Determine if a point is inside the circle's bounds
           var radiusMax = parseInt(radius) + parseInt(sliderH2);
           var radiusMin = radius - sliderH2;
-          console.log('bool', radiusMax);                
+                         
           return ((Math.sqrt((x-x0)*(x-x0) + (y-y0)*(y-y0))) <= radiusMax) && (Math.sqrt((x-x0)*(x-x0) + (y-y0)*(y-y0)) >= radiusMin);
-          
         }
 
         function whichCircle (event) {
-          var x = event.clientX-elPos.x;
-          var y = event.clientY-elPos.y;
+              var x = event.clientX-elPos.x;
+              var y = event.clientY-elPos.y;
 
-          
-          for (i=0; i < options.length; i++) {
-            var radiusMax = parseInt(options[i].radius) + parseInt(sliderH2);
-            var radiusMin = options[i].radius - sliderH2;
-           
-            if (((Math.sqrt((x-x0)*(x-x0) + (y-y0)*(y-y0))) <= radiusMax) && (Math.sqrt((x-x0)*(x-x0) + (y-y0)*(y-y0)) >= radiusMin)) {
-              radius = options[i].radius;
               
-           
-              
-            }
-          }
-          return radius;
-        }
-   
-     
-       
-        var mouseHandler = function(event, radius) {
+              for (i=0; i < options.length; i++) {
+                var radiusMax = parseInt(options[i].radius) + parseInt(sliderH2);
+                var radiusMin = options[i].radius - sliderH2;
+               
+                if (((Math.sqrt((x-x0)*(x-x0) + (y-y0)*(y-y0))) <= radiusMax) && (Math.sqrt((x-x0)*(x-x0) + (y-y0)*(y-y0)) >= radiusMin)) {
+                  radius = options[i].radius;
+                }     
+              } 
+              return radius;  
+            }   
+
+        function mouseHandler(event, radius) {
+            var mPos = {x: event.clientX-elPos.x-(x0-radius), y: event.clientY-elPos.y-(x0-radius)}; // polozaj x in y koordinate miske
+            var index = options.findIndex(x => x.radius==radius);
                           
-                          var mPos = {x: event.clientX-elPos.x-(x0-radius), y: event.clientY-elPos.y-(x0-radius)}; // polozaj x in y koordinate miske
-                          var index = options.findIndex(x => x.radius==radius);
-                          console.log('mouseposition', mPos.x, mPos.y);
-
-                           if (circleContains(event.clientX-elPos.x, event.clientY-elPos.y)) {
-                                                                
-                                                    
+            if (circleContains(event.clientX-elPos.x, event.clientY-elPos.y)) {
+                
+                var atan = Math.atan2(mPos.x-radius, mPos.y-radius); // Math.atan2() vrne kot med x osjo in točko (x,y)                                                
+                deg = -atan/(Math.PI/180) + 180; // final (0-360 positive) degrees from mouse position                                    
                              
-                             console.log('client', event.clientX, event.clientY);
-                             console.log('mouseposition', mPos.x, mPos.y);
-                             var atan = Math.atan2(mPos.x-radius, mPos.y-radius); // Math.atan2() vrne kot med x osjo in točko (x,y)
-                             console.log('atan', atan);
-                             deg = -atan/(Math.PI/180) + 180; // final (0-360 positive) degrees from mouse position 
-                             console.log('-atan/(Math.PI/180)',-atan/(Math.PI/180));
-                             console.log('degrees', deg);
-
+                var part = -0.5*Math.PI + deg * (Math.PI/180);
+                X = Math.round(radius* Math.sin(deg*Math.PI/180));    
+                Y = Math.round(radius*  -Math.cos(deg*Math.PI/180));             
                              
-                            
-                             var part = -0.5*Math.PI + deg * (Math.PI/180);
-                             X = Math.round(radius* Math.sin(deg*Math.PI/180));    
-                             Y = Math.round(radius*  -Math.cos(deg*Math.PI/180));
+                function updateState() {
+                    options[index].part = part;
+                    options[index].sliderX = X;
+                    options[index].sliderY = Y;
+                    options[index].deg = Math.ceil(deg);                                       
+                }          
+                updateState();               
+                clear();             
 
-
-                             function updateState() {
-                                                           
-                               options[index].part = part;
-                               options[index].sliderX = X;
-                               options[index].sliderY = Y;
-                               options[index].deg = Math.ceil(deg);
-                               
-                             }
-
-                             updateState();
-                             
-
-                             clear();
-
-                             for (i=0; i < options.length; i++) {
+                for (i=0; i < options.length; i++) {
                               drawCircle(options[i].radius);
                               colorCircle(options[i].color, options[i].radius, options[i].part);
-                              
                               drawSlider(options[i].sliderX + x0, options[i].sliderY + y0); 
+                    }                          
+            }                  
+        } 
 
-                             }
-                             
-                               
-
-                             function displayValues(options, element) {
-                              var value = options.map(function(item, index) {
-                              return '<input id="value-'+ index +'" class="list-item" type="text" value="' + item.deg + 
-                              '" class="range" data-max="' + item.maxValue + '" data-min="' + item.minValue + '" data-step="' + item.step +
-                              '" name="angle">'
-                              });
-                              return  element.html(value); 
-                             }                   
-                            
-                             displayValues(options, $('#js-values'));
-
-                             for (i=0; i < options.length; i++) {
-                              $('input[id="value' + i +'"]').val();
-                             }
-                                                                     
-
-                           
-
-                                                       
-                          } 
-
-                          
-
-        }
-
-
-
-        $('#c-circle').click(function (event) { // later on add mousemove, mouseup and mousedown
+         $('#c-circle').click(function (event) { // later on add mousemove, mouseup and mousedown
                         event.preventDefault();
                         event.stopPropagation();
                         
@@ -312,18 +230,73 @@ var moveSlider = function(element) {
                            mouseHandler(event, radius);     
                      
                         }
-                    });   
+                    });                               
+}
 
-      } 
+document.body.onload = function(radius) {
+
+  drawCircle(radius);
+  drawSlider(x0, y0 - radius); 
+
+};
 
 for (i=0; i < options.length; i++) {
   console.log('length', options.length);
   document.body.onload(options[i].radius);
-
-  
 }
 
-moveSlider($('#c-circle'));
+moveSlider($('#c-circle'));                             
+                            
+                             
+
+
+                             
+                             
+
+                             
+
+                             
+                             
+                               
+
+                        
+                                                                     
+
+                           
+
+                                                       
+                                         
+                          
+
+                          
+
+        
+        
+       
+        
+        
+        
+
+
+      
+        
+       
+     
+        
+
+        
+   
+     
+       
+        
+
+
+
+       
+
+       
+
+
 
 
 
